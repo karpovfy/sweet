b54390180a91b975ba566c5eea67a49ec38fef86
karpovfy <karpovfy@gmail.com>
2018-10-19 20:29:44 +0800

    migrate to freemarker

--- src/main/java/com/fyodork/hello/controller/MainController.java
+++ src/main/java/com/fyodork/hello/controller/MainController.java
@@ -33,11 +33,19 @@ public class MainController {
 
     @GetMapping("/main")
 
-    public String main(Map<String,Object> model)
+    public String main(@RequestParam(required = false,defaultValue = "")  String filter,Model model)
     {
 
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages",messages);
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -58,7 +66,7 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
+ /*   @PostMapping("filter")
 
     public String filter(
             @RequestParam String filter,Map<String,Object> model)
@@ -67,17 +75,10 @@ public class MainController {
 
         Iterable<Message> messages;
 
-        if (filter!=null || !filter.isEmpty()) {
-           messages = messageRepo.findByTag(filter);
 
-        }
-        else
-        {
-            messages=messageRepo.findAll();
-        }
         model.put("messages", messages);
         return "main";
-    }
+    }*/
 
 
 }
---
+++ src/main/resources/templates/greeting.ftl
@@ -0,0 +1,11 @@
+<#import "parts/common.ftl" as c>
+
+<@c.page>
+
+<div>
+Прiвет+
+</div>
+
+<a href="/main">main page</a>
+
+</@c.page>
